2417ac64c0cef7962a52fcebe0dcb988a21691e3
Kai Sun <bryant.kai1227@gmail.com>
2022-10-27 17:32:46 -0400

    Initial commit

---
+++ src/main/java/com/luckystar/macdrop/MacDropApplication.java
@@ -0,0 +1,13 @@
+package com.luckystar.macdrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacDropApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MacDropApplication.class, args);
+    }
+
+}
